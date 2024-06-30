367bb24637e16b50e36b18e4726352ceed34598c
Deepikapatel <deepikapatel5687@gmail.com>
2024-07-01 00:28:19 +0530

    add commit changes

--- component/src/App.jsx
+++ component/src/App.jsx
@@ -1,33 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+const age=24;
+let name="Deepika";
+const sub=<ol>
+  <li>PHP</li>
+  <li>JAVA</li>
+  <li>Oracal</li>
+  <li>ASP</li>
+</ol>
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello i am {age} year old!!!! my name {name}</h1>
+      <h1>hello i am {age*3+4} year old!!! my name {name}</h1>
+      <h1> My Subject : {sub}</h1>
     </>
   )
 }
