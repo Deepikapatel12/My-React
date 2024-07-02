749043ed51ad170654ab094f20b1bdbd5308aeca
Deepikapatel <deepikapatel5687@gmail.com>
2024-07-03 00:31:03 +0530

    map function

--- mapfunction/src/App.jsx
+++ mapfunction/src/App.jsx
@@ -8,12 +8,39 @@ const mynm=name.map((key)=>{
         </>
     )
 });
+const ans=students.map((key1)=>{
+    return(
+      <>
+        
+        <tr>
+          <td>{key1.rollno}</td>
+          <td>{key1.name}</td>
+          <td>{key1.city}</td>
+        </tr>
+      </>
+    )
+  });
 const App=()=>{
     return(
         <>
             <h1>Welcome to map method</h1>
             {mynm}
+
+            <table border="1" width="600">
+    <tr>
+      <th>Roll NO </th>
+      <th>Name</th>
+      <th>City</th>
+    </tr>
+    {ans}
+  </table>
+            
         </>
     )
 }
 export default App;
+
+
+
+
+// =================================================================================================================================
--- mapfunction/src/Map.jsx
+++ mapfunction/src/Map.jsx
@@ -15,3 +15,33 @@
 //     )
 // }
 // export default App;
+
+
+const students=[
+    {
+      "rollno":120,
+      "name":"Deepu",
+      "city":"bhopal"
+    },
+    {
+      "rollno":121,
+      "name":"megha",
+      "city":"chindwara"
+    },
+    {
+      "rollno":122,
+      "name":"varsha",
+      "city":"bhopal"
+    },
+    {
+      "rollno":123,
+      "name":"kumkum",
+      "city":"bhopal"
+    },
+    {
+      "rollno":124,
+      "name":"Tanishka",
+      "city":"bhopal"
+    }
+  ]
+  export default students;
