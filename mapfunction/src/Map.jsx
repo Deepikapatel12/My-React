32fddd51b3e26aea6af568fa994f2cf726bbb6aa
Deepikapatel <deepikapatel5687@gmail.com>
2024-07-02 12:57:38 +0530

    map and arrow function

---
+++ mapfunction/src/Map.jsx
@@ -0,0 +1,17 @@
+// const name=["sanju","manju","ranju","anju","dkjd"]
+// const mynm=name.map((key)=>{
+//     return(
+//         <>
+//             <h1>{key}</h1>
+//         </>
+//     )
+// });
+// const App=()=>{
+//     return(
+//         <>
+//             <h1>Welcome to map method</h1>
+//             {mynm}
+//         </>
+//     )
+// }
+// export default App;
