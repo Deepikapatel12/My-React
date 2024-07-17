9b8e2df8d2ec90b58f0534bc915e319b8b669222
Deepikapatel <deepikapatel5687@gmail.com>
2024-07-17 10:56:47 +0530

    ReactBoot Strap

---
+++ React_BootStrap/src/App.jsx
@@ -0,0 +1,14 @@
+import Container from 'react-bootstrap/Container';
+import TopMenu from './components/TopMenu';
+
+const App=()=>{
+  return(
+    <>
+      <h1>Hello</h1>
+      
+     <TopMenu />
+  
+    </>
+  )
+}
+export default App;
