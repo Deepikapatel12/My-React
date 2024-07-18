0b4fb58c01e43e4d84def7c5125fe33a6e6dba19
Deepikapatel <deepikapatel5687@gmail.com>
2024-07-18 17:34:52 +0530

    Use Effect

--- Use_Effect/src/App.jsx
+++ Use_Effect/src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+// use effect hook
+const App=()=>{
+const [cnt, setCnt]=useState(0);
+useEffect(()=>{
+// 1st type
+// runs on every render
+  setTimeout(()=>{
+    setCnt(cnt+1)
+  }, 3000);
 
-function App() {
-  const [count, setCount] = useState(0)
+})
+// 2nd type
+const [cnt1, setCnt1]=useState(0);
+useEffect(()=>{
+  // runs only one render
+  setTimeout(()=>{
+    setCnt1(cnt1+1);
+  }, 3000)
+}, [])
 
-  return (
+// 3rd type
+const [cnt2, setCnt2]=useState(0);
+const [multi, setMulti]=useState(0);
+useEffect(()=>{
+  setMulti(cnt2*2)
+  // and any time any dependency value changes
+}, [cnt2])
+
+// 4th type
+
+
+
+
+  return(
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+<h1>Welcome to use effect hook</h1>
+
+<h2>Use Effect Hook 1st type : {cnt}</h2>
+
+
+<h2>Use Effect Hook 2nd Type :{cnt1}</h2>
+
+
+<h2>Use Effect Hook 3rd Type :{cnt2}</h2>
+<h2>Use Effect Hook Multiplication 3rd Type :{multi}</h2>
+<button onClick={()=>{setCnt2(cnt2+1)}}>Click Here</button>
+
+
     </>
   )
 }
-
-export default App
+export default App;
