787ca08c978ccdcc481fa7ad4c5dafa91c0050d8
Deepikapatel <deepikapatel5687@gmail.com>
2024-07-21 01:21:51 +0530

    Form with json

--- Form_with_Json/src/App.jsx
+++ Form_with_Json/src/App.jsx
@@ -2,10 +2,29 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
+import Home from "./Component/Home";
+import Insert from "./Component/Insert";
+import Display from "./Component/Display";
+import Search from "./Component/Search";
+import Update from "./Component/Update";
+import Contact from "./Component/Contact";
 const App=()=>{
   return(
     <>
-<h1>Welcome</h1>
+<BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Layout/>}>
+    <Route index element={<Home/>}/>
+    <Route path="home" element={<Home/>}/>
+    <Route path="insert" element={<Insert/>}/>
+    <Route path="display" element={<Display/>}/>
+    <Route path="search" element={<Search/>}/>
+    <Route path="update" element={<Update/>}/>
+    <Route path="contact" element={<Contact/>}/>
+
+    </Route>
+  </Routes>
+</BrowserRouter>
     </>
   )
 }
