70b51b905f21ec87502f1b04b2e6b879aa69692d
Deepikapatel <deepikapatel5687@gmail.com>
2024-09-25 23:35:59 +0530

    adding a folder by name deepika

---
+++ deepika/src/App.jsx
@@ -0,0 +1,11 @@
+import Cybrom from "./Cybrom";
+
+const App=()=>{
+  return(
+    <>
+      <h1>Hello I Am Deepika Patel From Bhopal</h1>
+   <Cybrom/>
+    </>
+  )
+}
+export default App; 
---
+++ deepika/src/Cybrom.jsx
@@ -0,0 +1,6 @@
+const Cybrom=()=>{
+    return(
+        <h1>Welcome to Cybrom we are react students</h1>
+    )
+}
+export default Cybrom;
