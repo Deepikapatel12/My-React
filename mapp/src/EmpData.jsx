4a2bd942ce0a8175063d194a04e97db99991e03e
Deepikapatel <deepikapatel5687@gmail.com>
2024-10-01 23:18:26 +0530

    adding map function in the file

---
+++ mapp/src/EmpData.jsx
@@ -0,0 +1,22 @@
+
+const EmpData=[
+    {
+        "eno":501,
+        "name":"deepu",
+        "dept":"finance",
+        "salary":80000
+    },
+    {
+        "eNo":502,
+        "name":"deepu",
+        "dept":"finance",
+        "salary":80000
+    },
+    {
+        "eNo":503,
+        "name":"deepu",
+        "dept":"finance",
+        "salary":80000
+    }
+]
+export default EmpData;
---
+++ mapp/src/EmpDesign.jsx
@@ -0,0 +1,14 @@
+
+const EmpDesigh=(props)=>{
+    return(
+        <>
+            <tr>
+                <td>{props.eno}</td>
+                <td>{props.nm}</td>
+                <td>{props.dpt}</td>
+                <td>{props.sal}</td>
+            </tr>
+        </>
+    )
+}
+export default EmpDesigh;
